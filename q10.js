f4afd006f56d49f16f8531e93020e7e7874087ed
guy586 <guy586@gmail.com>
2021-01-20 15:21:13 +0200

    solution to q10

---
+++ q10.js
@@ -0,0 +1,13 @@
+// the function prints the elements of the following multi-dimensional array.
+function print(array){
+   for (let i = 0; i < array.length; i++) {
+       console.log("row " + i);
+       for (let j = 0; j < array[i].length; j++) {
+           console.log(array[i][j]);   
+       } 
+   }
+}
+
+let a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+print(a);
