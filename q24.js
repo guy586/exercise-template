92dff3c7841dfa7d6ea4b36d36633dcd209c45c8
guy586 <guy586@gmail.com>
2021-01-20 22:53:44 +0200

    solution to q24

---
+++ q24.js
@@ -0,0 +1,12 @@
+
+function removeUn(arr) {
+    let n = [];
+    for (let i = 0 ; i < arr.length ; i++) {
+        if (arr[i] !== null && arr[i] !== 0 && arr[i] !== '' && arr[i] !== false && arr[i] !== undefined && !isNaN(arr[i])) {
+            n.push(arr[i]);
+        }
+    }
+    return n;
+}
+
+console.log(removeUn([NaN, 0, 15, false, -22, '',undefined, 47, null]));
