0c1757d83bc4897d380413369eefe5ab4fb8962b
guy586 <guy586@gmail.com>
2021-01-20 21:56:58 +0200

    comment + adjustment

--- q20.js
+++ q20.js
@@ -1,8 +1,7 @@
-let duplicate = [];
-
-
+// the function finds duplicate values in an array.
 function dupValues(array) {
-   for (let i = 0; i < array.length; i++) {
+    let duplicate = [];
+    for (let i = 0; i < array.length; i++) {
       for (let j = i + 1 ; j < array.length; j++) {
         if (array[i] === array[j] && !duplicate.includes(array[i])) {
             duplicate.push(array[i]);
