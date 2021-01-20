4f94c61a6ff4cfa635ed223d5f58dca708051562
guy586 <guy586@gmail.com>
2021-01-20 21:07:01 +0200

    comment

--- q7.js
+++ q7.js
@@ -1,6 +1,6 @@
 let array = [ 3, 8, 7, 6, 5, -4, -3, 2, 1 ];
 console.log(sortByNumer(array));
-
+// the function sort the items of an array.
 function sortByNumer(arr){
     let holder = 0;
     for (let i = 0 ; i < arr.length ; i++) {
