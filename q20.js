4dfedc5d6cc911013f836f2f1e16d6c9ebff5b47
guy586 <guy586@gmail.com>
2021-01-20 21:55:44 +0200

    solution to q20

---
+++ q20.js
@@ -0,0 +1,15 @@
+let duplicate = [];
+
+
+function dupValues(array) {
+   for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1 ; j < array.length; j++) {
+        if (array[i] === array[j] && !duplicate.includes(array[i])) {
+            duplicate.push(array[i]);
+        }
+      }
+   }
+   return duplicate;
+}
+
+console.log(dupValues([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
